b3bba0341bc2369d9931cf257c1d211a63e68e09
Murilo Ribeiro <murilo.r.freitas@hotmail.com>
2023-05-12 21:19:12 -0300

    upload

---
+++ lista 1/2/dinheiro.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int cem, cinquenta, vinte, dez, cinco, dois;
+    int um, meio, quarto, decimo, vigesimo, centavo;
+    double quantia;
+    
+    
+
+    return 0;
+}
